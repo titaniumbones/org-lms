2921226d19f96396d671ff5393d9e2a7193c30dc
Matt Price <matt.price@utoronto.ca>
2025-03-26 20:19:15 -0400

    various uncommitted changes from Sept 2023

--- ox-canvashtml.el
+++ ox-canvashtml.el
@@ -13,7 +13,7 @@
                          :store #'org-canvas-store-link)
 
 (defcustom org-canvas-command 'canvas
-  "The Emacs command to be used to display a man page."
+  "The Emacs command to be used to display a canvas page."
   :group 'org-link
   :type '(choice (const man) (const woman)))
 
@@ -112,6 +112,318 @@ or filename if no title."
   )
 ;; define the derived backend:1 ends here
 
+;; [[file:ox-canvashtml.org::*Replace the link function so we can update local image links at least][Replace the link function so we can update local image links at least:1]]
+(defun org-canvashtml-link (link desc info)
+  "Transcode a LINK object from Org to HTML.
+DESC is the description part of the link, or the empty string.
+INFO is a plist holding contextual information.  See
+`org-export-data'."
+  (let* ((html-ext (plist-get info :html-extension))
+	 (dot (when (> (length html-ext) 0) "."))
+	 (link-org-files-as-html-maybe
+	  (lambda (raw-path info)
+	    ;; Treat links to `file.org' as links to `file.html', if
+	    ;; needed.  See `org-html-link-org-files-as-html'.
+	    (cond
+	     ((and (plist-get info :html-link-org-files-as-html)
+		   (string= ".org"
+			    (downcase (file-name-extension raw-path "."))))
+	      (concat (file-name-sans-extension raw-path) dot html-ext))
+	     (t raw-path))))
+	 (type (org-element-property :type link))
+	 (raw-path (org-element-property :path link))
+	 ;; Ensure DESC really exists, or set it to nil.
+	 (desc (org-string-nw-p desc))
+	 (path
+	  (cond
+	   ((member type '("http" "https" "ftp" "mailto" "news"))
+	    (url-encode-url (concat type ":" raw-path)))
+	   ((string= "file" type)
+	    ;; During publishing, turn absolute file names belonging
+	    ;; to base directory into relative file names.  Otherwise,
+	    ;; append "file" protocol to absolute file name.
+	    (setq raw-path
+		  (org-export-file-uri
+		   (org-publish-file-relative-name raw-path info)))
+	    ;; Possibly append `:html-link-home' to relative file
+	    ;; name.
+	    (let ((home (and (plist-get info :html-link-home)
+			     (org-trim (plist-get info :html-link-home)))))
+	      (when (and home
+			 (plist-get info :html-link-use-abs-url)
+			 (file-name-absolute-p raw-path))
+		(setq raw-path (concat (file-name-as-directory home) raw-path))))
+	    ;; Maybe turn ".org" into ".html".
+	    (setq raw-path (funcall link-org-files-as-html-maybe raw-path info))
+	    ;; Add search option, if any.  A search option can be
+	    ;; relative to a custom-id, a headline title, a name or
+	    ;; a target.
+	    (let ((option (org-element-property :search-option link)))
+	      (if (not option) raw-path
+		(let ((path (org-element-property :path link)))
+		  (concat raw-path
+			  "#"
+			  (org-publish-resolve-external-link option path t))))))
+	   (t raw-path)))
+	 (attributes-plist
+	  (org-combine-plists
+	   ;; Extract attributes from parent's paragraph.  HACK: Only
+	   ;; do this for the first link in parent (inner image link
+	   ;; for inline images).  This is needed as long as
+	   ;; attributes cannot be set on a per link basis.
+	   (let* ((parent (org-export-get-parent-element link))
+		  (link (let ((container (org-export-get-parent link)))
+			  (if (and (eq 'link (org-element-type container))
+				   (org-html-inline-image-p link info))
+			      container
+			    link))))
+	     (and (eq link (org-element-map parent 'link #'identity info t))
+		  (org-export-read-attribute :attr_html parent)))
+	   ;; Also add attributes from link itself.  Currently, those
+	   ;; need to be added programmatically before `org-html-link'
+	   ;; is invoked, for example, by backends building upon HTML
+	   ;; export.
+	   (org-export-read-attribute :attr_html link)))
+	 (attributes
+	  (let ((attr (org-html--make-attribute-string attributes-plist)))
+	    (if (org-string-nw-p attr) (concat " " attr) ""))))
+    (cond
+     ;; Link type is handled by a special function.
+     ((org-export-custom-protocol-maybe link desc 'html info))
+     ;; Image file.
+     ((and (plist-get info :html-inline-images)
+	   (org-export-inline-image-p
+	    link (plist-get info :html-inline-image-rules)))
+      (org-html--format-image path attributes-plist info))
+     ;; Radio target: Transcode target's contents and use them as
+     ;; link's description.
+     ((string= type "radio")
+      (let ((destination (org-export-resolve-radio-link link info)))
+	(if (not destination) desc
+	  (format "<a href=\"#%s\"%s>%s</a>"
+		  (org-export-get-reference destination info)
+		  attributes
+		  desc))))
+     ;; Links pointing to a headline: Find destination and build
+     ;; appropriate referencing command.
+     ((member type '("custom-id" "fuzzy" "id"))
+      (let ((destination (if (string= type "fuzzy")
+			     (org-export-resolve-fuzzy-link link info)
+			   (org-export-resolve-id-link link info))))
+	(pcase (org-element-type destination)
+	  ;; ID link points to an external file.
+	  (`plain-text
+	   (let ((fragment (concat "ID-" path))
+		 ;; Treat links to ".org" files as ".html", if needed.
+		 (path (funcall link-org-files-as-html-maybe
+				destination info)))
+	     (format "<a href=\"%s#%s\"%s>%s</a>"
+		     path fragment attributes (or desc destination))))
+	  ;; Fuzzy link points nowhere.
+	  (`nil
+	   (format "<i>%s</i>"
+		   (or desc
+		       (org-export-data
+			(org-element-property :raw-link link) info))))
+	  ;; Link points to a headline.
+	  (`headline
+	   (let ((href (org-html--reference destination info))
+		 ;; What description to use?
+		 (desc
+		  ;; Case 1: Headline is numbered and LINK has no
+		  ;; description.  Display section number.
+		  (if (and (org-export-numbered-headline-p destination info)
+			   (not desc))
+		      (mapconcat #'number-to-string
+				 (org-export-get-headline-number
+				  destination info) ".")
+		    ;; Case 2: Either the headline is un-numbered or
+		    ;; LINK has a custom description.  Display LINK's
+		    ;; description or headline's title.
+		    (or desc
+			(org-export-data
+			 (org-element-property :title destination) info)))))
+	     (format "<a href=\"#%s\"%s>%s</a>" href attributes desc)))
+	  ;; Fuzzy link points to a target or an element.
+	  (_
+           (if (and destination
+                    (memq (plist-get info :with-latex) '(mathjax t))
+                    (eq 'latex-environment (org-element-type destination))
+                    (eq 'math (org-latex--environment-type destination)))
+               ;; Caption and labels are introduced within LaTeX
+	       ;; environment.  Use "ref" or "eqref" macro, depending on user
+               ;; preference to refer to those in the document.
+               (format (plist-get info :html-equation-reference-format)
+                       (org-html--reference destination info))
+             (let* ((ref (org-html--reference destination info))
+                    (org-html-standalone-image-predicate
+                     #'org-html--has-caption-p)
+                    (counter-predicate
+                     (if (eq 'latex-environment (org-element-type destination))
+                         #'org-html--math-environment-p
+                       #'org-html--has-caption-p))
+                    (number
+		     (cond
+		      (desc nil)
+		      ((org-html-standalone-image-p destination info)
+		       (org-export-get-ordinal
+			(org-element-map destination 'link #'identity info t)
+			info 'link 'org-html-standalone-image-p))
+		      (t (org-export-get-ordinal
+			  destination info nil counter-predicate))))
+                    (desc
+		     (cond (desc)
+			   ((not number) "No description for this link")
+			   ((numberp number) (number-to-string number))
+			   (t (mapconcat #'number-to-string number ".")))))
+               (format "<a href=\"#%s\"%s>%s</a>" ref attributes desc)))))))
+     ;; Coderef: replace link with the reference name or the
+     ;; equivalent line number.
+     ((string= type "coderef")
+      (let ((fragment (concat "coderef-" (org-html-encode-plain-text path))))
+	(format "<a href=\"#%s\" %s%s>%s</a>"
+		fragment
+		(format "class=\"coderef\" onmouseover=\"CodeHighlightOn(this, \
+'%s');\" onmouseout=\"CodeHighlightOff(this, '%s');\""
+			fragment fragment)
+		attributes
+		(format (org-export-get-coderef-format path desc)
+			(org-export-resolve-coderef path info)))))
+     ;; External link with a description part.
+     ((and path desc)
+      (format "<a href=\"%s\"%s>%s</a>"
+	      (org-html-encode-plain-text path)
+	      attributes
+	      desc))
+     ;; External link without a description part.
+     (path
+      (let ((path (org-html-encode-plain-text path)))
+	(format "<a href=\"%s\"%s>%s</a>" path attributes path)))
+     ;; No path, only description.  Try to do something useful.
+     (t
+      (format "<i>%s</i>" desc)))))
+
+
+(defun org-canvashtml--format-image (source attributes info)
+  "Return \"img\" tag with given SOURCE and ATTRIBUTES.
+SOURCE is a string specifying the location of the image.
+ATTRIBUTES is a plist, as returned by
+`org-export-read-attribute'.  INFO is a plist used as
+a communication channel."
+  ;; (org-html--svg-image source attributes info)
+  (org-html-close-tag
+   "img"
+   (org-html--make-attribute-string
+    (org-combine-plists
+     (list :src source
+	   :alt (if (string-match-p "^ltxpng/" source)
+		    (org-html-encode-plain-text
+		     (org-find-text-property-in-string 'org-latex-src source))
+		  (file-name-nondirectory source)))
+     attributes))
+   info))
+
+(defun org-canvashtml--image-rewrite-path (path info)
+  "Copy local images and pdfs to the static/bundle directory if needed.
+Also update the link paths to match those.
+
+PATH is the path to the image or any other attachment.
+
+INFO is a plist used as a communication channel."
+  ;; (message "[ox-hugo attachment DBG] The Hugo section is: %s" (plist-get info :hugo-section))
+  ;; (message "[ox-hugo attachment DBG] The Hugo base dir is: %s" (plist-get info :hugo-base-dir))
+  (let* ((path-unhexified (url-unhex-string path))
+         (path-true (file-truename path-unhexified))
+         (dest-dir (or bundle-dir static-dir))
+         ret)
+    (unless (file-directory-p static-dir)
+      (user-error "Please create the %s directory" static-dir))
+    (if (and (file-exists-p path-true)
+             (member (file-name-extension path-unhexified) exportables)
+             (file-directory-p dest-dir))
+        (progn
+          ;; Check if `path-true' is already inside `dest-dir'.
+          (if (string-match (regexp-quote dest-dir) path-true)
+              (progn
+                ;; If so, return *only* the path considering the
+                ;; destination directory as root.
+                (setq ret (concat "/" (substring path-true (match-end 0)))))
+            (let* ((file-name-relative-path
+                    (cond
+                     ((string-match "/static/" path-true)
+                      ;; `path-true' is "/foo/static/bar/baz.png",
+                      ;; return "bar/baz.png".
+                      ;; (message "[ox-hugo DBG attch rewrite] path contains static")
+                      ;; If path-true contains "/static/", set the
+                      ;; `dest-dir' to `static-dir' (even if this is a
+                      ;; page bundle).
+                      (setq dest-dir static-dir)
+                      (substring path-true (match-end 0)))
+                     (bundle-dir
+                      (cond
+                       ((string-match (concat "/" (regexp-quote bundle-name) "/") path-true)
+                        ;; This is a page bundle.  `bundle-name' is
+                        ;; "<BUNDLE_NAME>", `path-true' is
+                        ;; "<ORG_FILE_DIR>/bar/<BUNDLE_NAME>/zoo/baz.png",
+                        ;; return "zoo/baz.png".
+                        ;; (message "[ox-hugo DBG attch rewrite BUNDLE 1] bundle-name: %s" bundle-name)
+                        ;; (message "[ox-hugo DBG attch rewrite BUNDLE 1] attch along with Org content: %s"
+                        ;;          (substring path-true (match-end 0)))
+                        (substring path-true (match-end 0)))
+                       ((string-match (regexp-quote default-directory) path-true)
+                        ;; This is a page bundle.  `default-path' is
+                        ;; "<ORG_FILE_DIR>/", `path-true' is
+                        ;; "<ORG_FILE_DIR>/bar/baz.png", return
+                        ;; "bar/baz.png".
+                        ;; (message "[ox-hugo DBG attch rewrite BUNDLE 2] attch along with Org content: %s"
+                        ;;          (substring path-true (match-end 0)))
+                        (substring path-true (match-end 0)))
+                       (t
+                        ;; This is a page bundle.  `default-path' is
+                        ;; "<ORG_FILE_DIR>/", `path-true' is
+                        ;; "/foo/bar/baz.png", return "baz.png".
+                        ;; (message "[ox-hugo DBG attch rewrite BUNDLE 3] attch neither in static nor in Org file dir")
+                        (file-name-nondirectory path-unhexified))))
+                     (t
+                      ;; Else, `path-true' is "/foo/bar/baz.png",
+                      ;; return "ox-hugo/baz.png".  "ox-hugo" is the
+                      ;; default value of
+                      ;; `org-hugo-default-static-subdirectory-for-externals'.
+                      ;; (message "[ox-hugo DBG attch rewrite] neither BUNDLE nor contains static")
+                      (concat
+                       (file-name-as-directory org-hugo-default-static-subdirectory-for-externals)
+                       (file-name-nondirectory path-unhexified)))))
+                   (dest-path (concat dest-dir file-name-relative-path))
+                   (dest-path-dir (file-name-directory dest-path)))
+              ;; The `dest-dir' would already exist.  But if
+              ;; `file-name-relative-path' is "images/image.png" or
+              ;; "foo/bar.txt", it's likely that "`dest-dir'/images"
+              ;; or "`dest-dir'/foo" might not exist.  So create those
+              ;; if needed below.
+              (unless (file-exists-p dest-path-dir)
+                (mkdir dest-path-dir :parents))
+              ;; (message "[ox-hugo DBG attch rewrite] file-name-relative-path: %s" file-name-relative-path)
+              ;; (message "[ox-hugo DBG attch rewrite] dest-path: %s" dest-path)
+              ;; (message "[ox-hugo DBG attch rewrite] dest-path-dir: %s" dest-path-dir)
+
+              ;; Do the copy only if the file to be copied is newer or
+              ;; doesn't exist in the static dir.
+              (when (file-newer-than-file-p path-true dest-path)
+                (message "[ox-hugo] Copied %S to %S" path-true dest-path)
+                (copy-file path-true dest-path :ok-if-already-exists))
+              (setq ret (if (and bundle-dir
+                                 (string= bundle-dir dest-dir))
+                            ;; If attachments are copied to the bundle
+                            ;; directory, don't prefix the path as "/"
+                            ;; as those paths won't exist at the site
+                            ;; base URL.
+                            file-name-relative-path
+                          (concat "/" file-name-relative-path))))))
+      (setq ret path))
+    ;; (message "[ox-hugo DBG attch rewrite] returned path: %s" ret)
+    ret))
+;; Replace the link function so we can update local image links at least:1 ends here
+
 ;; [[file:ox-canvashtml.org::*Replace the section function][Replace the section function:1]]
 ;;;; Section
 
@@ -200,6 +512,8 @@ holding contextual information."
                           org-canvas-html-fat-classes))
            (add-toggler (or (org-element-property :CANVAS_HTML_TOGGLE headline)
                             (org-export-read-attribute :attr_canvashtml headline :toggle)))
+           (show-toggled (or (org-element-property :CANVAS_HTML_SHOW headline)
+                             (org-export-read-attribute :attr_canvashtml headline :show)))
 	   (formatted-text
 	    (if (plist-get info :html-self-link-headlines)
 		(format "<a href=\"#%s\">%s</a>" id full-text)
@@ -262,7 +576,7 @@ holding contextual information."
                   (concat
                    (format "<div id=\"contents-%s\"%s>"
                            id
-                           (when add-toggler " style=\"display:none\""))
+                           (when (and add-toggler (not show-toggled)) " style=\"display:none\""))
                    (if (eq (org-element-type first-content) 'section) contents
                      (concat (org-canvas-html-section first-content "" info) contents))
                    "</div>")
@@ -275,7 +589,7 @@ holding contextual information."
   "Since <head> will in any case be stripped out,
 return just the body with an extra CSS tag"
   ;; code statically for now
-  (let* ((rawHtml  (concat ;;"<link rel=\"stylesheet\" type=\"text/css\" href=\""
+  (let* ((rawHtml  (concat "<link rel=\"stylesheet\" type=\"text/css\" href=\""
                     org-canvas-html-css-file  "\" \\>\n "
                            ;; Document contents.
                            (let ((div (assq 'content (plist-get info :html-divs))))
@@ -307,7 +621,7 @@ return just the body with an extra CSS tag"
                            (format "</%s>\n" (nth 1 (assq 'content (plist-get info :html-divs))))
                            ))
          (tempFile (make-temp-file "canvas-html-export" nil ".html" rawHtml)))
-    (call-process "juice" nil "*juice-process*" nil "--css" "/home/matt/IFP100/extra-styles.css" tempFile tempFile)
+    (call-process "juice" nil "*juice-process*" nil "--css" org-canvas-html-css-file tempFile tempFile)
     (with-temp-buffer
       (insert-file-contents tempFile)
       (buffer-string))))
@@ -326,7 +640,7 @@ holding export options."
            ;; Footnotes section.
            (org-html-footnote-section info)))
          (tempFile (make-temp-file "canvas-html-export" nil ".html" rawHtml)))
-    (call-process "juice" nil "*juice-process*" nil "--css" "/home/matt/IFP100/extra-styles.css" tempFile tempFile)
+    (call-process "juice" nil "*juice-process*" nil "--css" org-canvas-html-css-file tempFile tempFile)
     (with-temp-buffer
       (insert-file-contents tempFile)
       (buffer-string))))
